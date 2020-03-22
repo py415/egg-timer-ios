4f13a3aeafa622922074a636156e2d4d1f6b654b
Philip Yu <philipyu1997@gmail.com>
2020-03-21 19:53:39 -0700

    Add progress bar for the time remaining before egg is done

--- EggTimer/Controller/ViewController.swift
+++ EggTimer/Controller/ViewController.swift
@@ -12,26 +12,32 @@ class ViewController: UIViewController {
 
     // Properties
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
-    var secondsRemaining = 60
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     
     // Outlets
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        
         timer.invalidate()
         
         let hardness = sender.currentTitle! // Soft, Medium, Hard
+        totalTime = eggTimes[hardness]!
         
-        secondsRemaining = eggTimes[hardness]!
+        // Reset settings for timer
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             print("Your eggs are now done!")
