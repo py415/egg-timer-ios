74144e388c4baba2e477ccbe4c58e332aedb40a9
Philip Yu <philipyu1997@gmail.com>
2020-03-21 19:16:28 -0700

    Add egg countdown timer

--- EggTimer/Controller/ViewController.swift
+++ EggTimer/Controller/ViewController.swift
@@ -10,4 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Properties
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var secondsRemaining = 60
+    var timer = Timer()
+    
+    // Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        
+        secondsRemaining = eggTimes[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            print("Your eggs are now done!")
+            titleLabel.text = "Done!"
+        }
+    }
+    
 }
